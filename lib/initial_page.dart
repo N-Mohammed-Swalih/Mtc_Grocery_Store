1a0470e7a7ba3222201bd68880d706cc63610c48
N-Mohammed-Swalih <mohdswalih003@gmail.com>
2023-08-20 11:22:49 +0530

    commit

--- lib/initial_page.dart
+++ lib/initial_page.dart
@@ -6,52 +6,6 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(children: <Widget>[
-        Positioned(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Image.asset(
-            './assets/main.png',
-            fit: BoxFit.cover,
-          ),
-        ),
-        Positioned(
-          top: 600,
-          left: 52.5,
-          width: 260,
-          height: 50,
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => LoginPage()),
-              );
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Color(0xFFFFCF00),
-              alignment: Alignment.center,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(50),
-              ),
-            ),
-            child: const SizedBox(
-              width: 300,
-              child: Center(
-                child: Text(
-                  "Get Started!",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w800,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-      ]),
-    );
+    return Scaffold();
   }
 }
